9271d9df9d5fc21c2434a398c83bbe5b663d96a9
NotZeetaa <rodrigo2005contente@gmail.com>
2021-10-06 15:49:09 +0100

    anykernel: Add 66hz support
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -38,24 +38,29 @@ set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 dump_boot;
 
 case "$ZIPFILE" in
+  *66fps*|*66hz*)
+    ui_print "  • Setting 66 Hz refresh rate"
+    patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=1"
+    ;;
   *69fps*|*69hz*)
     ui_print "  • Setting 69 Hz refresh rate"
-    patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=1"
+    patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=2"
     ;;
   *72fps*|*72hz*)
     ui_print "  • Setting 72 Hz refresh rate"
-    patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=2"
+    patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=3"
     ;;
   *75fps*|*75hz*)
     ui_print "  • Setting 75 Hz refresh rate"
-    patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=3"
+    patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=4"
     ;;
   *)
     patch_cmdline "msm_drm.framerate_override" ""
     fr=$(cat /sdcard/framerate_override | tr -cd "[0-9]");
-    [ $fr -eq 69 ] && ui_print "  • Setting 69 Hz refresh rate" && patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=1"
-    [ $fr -eq 72 ] && ui_print "  • Setting 72 Hz refresh rate" && patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=2"
-    [ $fr -eq 75 ] && ui_print "  • Setting 75 Hz refresh rate" && patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=3"
+    [ $fr -eq 66 ] && ui_print "  • Setting 69 Hz refresh rate" && patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=1"
+    [ $fr -eq 69 ] && ui_print "  • Setting 69 Hz refresh rate" && patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=2"
+    [ $fr -eq 72 ] && ui_print "  • Setting 72 Hz refresh rate" && patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=3"
+    [ $fr -eq 75 ] && ui_print "  • Setting 75 Hz refresh rate" && patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=4"
     ;;
 esac
 
